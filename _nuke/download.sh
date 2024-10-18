ba25280f87ca121d0855111ea05163cfe47eef12
Minhyeok Park <pmh_only@pmh.codes>
2024-10-18 21:58:37 +0900

    feat: add mant uifhauiwehfuih

---
+++ _nuke/download.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget $(curl https://api.github.com/repos/ekristen/aws-nuke/releases/latest | jq '.assets[]|select(.name|endswith("linux-amd64.tar.gz")).browser_download_url' -r) -O- | tar zxvf -
