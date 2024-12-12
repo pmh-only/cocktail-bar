bce0e5d8a977aedd3a591d781fef28ed0669612d
Minhyeok Park <pmh_only@pmh.codes>
2024-12-12 19:16:06 +0900

    feat: add globalarch_1app

---
+++ _cocktails/1212_globalarch_1/infra/us/irsa.tf
@@ -0,0 +1,141 @@
+resource "aws_iam_policy" "sm" {
+  name = "project-policy-sm"
+  policy = data.aws_iam_policy_document.sm.json  
+}
+
+data "aws_iam_policy_document" "sm" {
+  statement {
+    actions = [
+      "secretsmanager:*"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+module "irsa" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "${var.project_name}-role-sm"
+
+  role_policy_arns = {
+    policy = aws_iam_policy.sm.arn
+  }
+
+  oidc_providers = {
+    cluster-oidc-provider = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = [
+        "default:sm"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "fluentd" {
+  name = "${var.project_name}-policy-fluentd"
+  policy = data.aws_iam_policy_document.fluentd.json  
+}
+
+data "aws_iam_policy_document" "fluentd" {
+  statement {
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogGroup",
+      "logs:PutRetentionPolicy",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+module "irsa2" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "${var.project_name}-role-fluentd"
+
+  role_policy_arns = {
+    policy = aws_iam_policy.fluentd.arn
+  }
+
+  oidc_providers = {
+    cluster-oidc-provider = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = [
+        "default:fluentd"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "ecr" {
+  name = "${var.project_name}-policy-ecr"
+  policy = data.aws_iam_policy_document.ecr.json  
+}
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    actions = [
+      "ecr:*",
+      "s3:*"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+module "irsa3" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "${var.project_name}-role-ecr"
+
+  role_policy_arns = {
+    policy = aws_iam_policy.ecr.arn
+  }
+
+  oidc_providers = {
+    cluster-oidc-provider = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = [
+        "default:ecr"
+      ]
+    }
+  }
+}
+
+# resource "aws_iam_policy" "prometheus" {
+#   name = "project-policy-prometheus"
+#   policy = data.aws_iam_policy_document.prometheus.json  
+# }
+
+# data "aws_iam_policy_document" "prometheus" {
+#   statement {
+#     actions = [
+#       "aps:RemoteWrite", 
+#       "aps:GetSeries", 
+#       "aps:GetLabels",
+#       "aps:GetMetricMetadata"
+#     ]
+
+#     resources = ["*"]
+#   }
+# }
+
+# module "irsa2" {
+#   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   role_name = "${var.project_name}-role-prometheus"
+
+#   role_policy_arns = {
+#     policy = aws_iam_policy.prometheus.arn
+#   }
+
+#   oidc_providers = {
+#     cluster-oidc-provider = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = [
+#         "opentelemetry-operator-system:adot-col-prom-metrics",
+#         "prometheus:amp-iamproxy-ingest-service-account"
+#       ]
+#     }
+#   }
+# }
---
+++ _cocktails/1212_globalarch_1/infra/us/vpc.tf
@@ -0,0 +1,32 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.project_name}-vpc"
+  cidr = "10.10.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.10.0.0/24", "10.10.1.0/24"]
+  private_subnets = ["10.10.10.0/24", "10.10.11.0/24"]
+  intra_subnets = ["10.10.20.0/24", "10.10.21.0/24"]
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "karpenter.sh/discovery" = "${var.project_name}-eks-cluster"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  public_subnet_names = ["${var.project_name}-public-subnet-a", "${var.project_name}-public-subnet-b"]
+  private_subnet_names = ["${var.project_name}-app-subnet-a", "${var.project_name}-app-subnet-b"]
+  intra_subnet_names = ["${var.project_name}-data-subnet-a", "${var.project_name}-data-subnet-b"]
+
+  enable_flow_log = true
+  create_flow_log_cloudwatch_iam_role = true
+  create_flow_log_cloudwatch_log_group = true
+
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  enable_nat_gateway = true  
+}
