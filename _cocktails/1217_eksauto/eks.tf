c2d95f70dba4bd8be960f83fa2548e9b96cf142a
Minhyeok Park <pmh_only@pmh.codes>
2024-12-17 17:46:40 +0900

    feat: add eks auto

---
+++ _cocktails/1217_eksauto/eks.tf
@@ -0,0 +1,45 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name    = "${var.project_name}-cluster"
+  cluster_version = "1.31"
+
+  cluster_compute_config = {
+    enabled    = true
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  access_entries = {
+    bastion = {
+      kubernetes_groups = []
+      principal_arn = aws_iam_role.bastion.arn
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            namespaces = []
+            type       = "cluster"
+          }
+        }
+      }
+    }
+    node = {
+      type = "EC2"
+      kubernetes_groups = ["system:nodes"]
+      principal_arn = "arn:aws:iam::648911607072:role/project-cluster-eks-auto-20241217040217307000000001"
+    }
+  }
+
+  cluster_enabled_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+}
