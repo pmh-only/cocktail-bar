fa2f9fc9f244a63e8f5931c46ea1c5a8e439ae3f
Minhyeok Park <pmh_only@pmh.codes>
2024-12-19 16:13:24 +0900

    feat: add logging solutions

--- kubernetes/cluster_provision/eks-addon.tf
+++ kubernetes/cluster_provision/eks-addon.tf
@@ -1,6 +1,6 @@
 module "eks_blueprints_addons" {
   source = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.0" #ensure to update this to the latest/desired version
+  version = "~> 1.0"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
@@ -20,15 +20,22 @@ module "eks_blueprints_addons" {
     kube-proxy = {
       most_recent = true
     }
+    amazon-cloudwatch-observability = {
+      most_recent = true
+    }
   }
 
   enable_karpenter = true
   enable_cert_manager = true
   enable_metrics_server = true
   enable_cluster_autoscaler = true
-  enable_bottlerocket_update_operator = true
   enable_aws_load_balancer_controller = true
   enable_external_secrets = true
+  enable_aws_for_fluentbit = true
+  enable_fargate_fluentbit = true
+  fargate_fluentbit = {
+    flb_log_cw = true
+  }
   
   aws_load_balancer_controller = {
     set = [
