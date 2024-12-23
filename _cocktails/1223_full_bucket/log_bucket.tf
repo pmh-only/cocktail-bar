f185f755cbc61f8836e9063e622e9ce75535844f
Minhyeok Park <pmh_only@pmh.codes>
2024-12-23 11:06:40 +0900

    feat: add full featured s3

---
+++ _cocktails/1223_full_bucket/log_bucket.tf
@@ -0,0 +1,8 @@
+module "s3_bucket_for_logs" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket_prefix = "${var.project_name}-logbucket"
+
+  force_destroy = true
+  attach_elb_log_delivery_policy = true
+}
+
