b70e1570d3d998eb571346ef1b920fe960b1c3c7
Minhyeok Park <pmh_only@pmh.codes>
2025-01-24 19:36:14 +0900

    refactor: change directory structure

--- networks/gwlb_suricata/suricata_build.sh
+++ networks/gwlb_suricata/suricata_build.sh
@@ -1,5 +1,5 @@
 amazon-linux-extras install -y epel && \
-  yum -y install git automake autoconf libtool gcc libpcap-devel pcre-devel libyaml-devel file-devel \
+  yum -y install iptables-services git automake autoconf libtool gcc libpcap-devel pcre-devel libyaml-devel file-devel \
   zlib-devel jansson-devel nss-devel libcap-ng-devel libnet-devel tar make \
   libnetfilter_queue-devel lua-devel PyYAML supervisor lz4-devel gzip && \
   curl -ks https://sh.rustup.rs -sSf | sh -s -- --profile minimal --default-toolchain 1.52.1 --no-modify-path -y && \
